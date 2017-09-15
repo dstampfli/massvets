ad97479a89209b374340e706371d50a36f1d5006
Dave Stampfli <david@stampfli.com>
2017-09-14 21:41:16 -0600

    Added test map using AGM

--- src/app/components/memorials-map/memorials-map.component.ts
+++ src/app/components/memorials-map/memorials-map.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memorials-map.component.css']
 })
 export class MemorialsMapComponent implements OnInit {
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
   constructor() { }
 
